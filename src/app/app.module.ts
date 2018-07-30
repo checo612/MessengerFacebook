68e4eb3d10d24154d445033c170dd1192287a7a4
Ricardo Alfonso Hernandez Sanchez <hernandez.ricardo@neoris.cxnetworks.net>
2018-07-30 16:02:24 -0500

    nothing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuariosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/usuarios/usuarios.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usuarios',
+  templateUrl: './usuarios.component.html',
+  styleUrls: ['./usuarios.component.css']
+})
+export class UsuariosComponent implements OnInit {
+  numbers;
+  constructor() {
+    this.numbers = Array(50).fill(0).map((x, i) => i);
+   }
+
+  ngOnInit() {
+  }
+
+}
