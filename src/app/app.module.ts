80f9fe42f781f359acfc1942a84aa5cc1067818b
checo612 <40772974+checo612@users.noreply.github.com>
2018-07-30 15:25:54 -0500

    agrege la barra para escribir el mensaje

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MensajesComponent } from './components/mensajes/mensajes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MensajesComponent
   ],
   imports: [
     BrowserModule,
