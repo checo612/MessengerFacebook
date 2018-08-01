86d380fbc8bef1523d1f2b62daa9c13057afb403
Ricardo Alfonso Hernandez Sanchez <hernandez.ricardo@neoris.cxnetworks.net>
2018-08-01 15:45:41 -0500

    Fixed styles

--- src/app/components/usuarios/usuarios.component.ts
+++ src/app/components/usuarios/usuarios.component.ts
@@ -5,17 +5,17 @@ import { Component, OnInit, ViewChildren, ViewChild } from '@angular/core';
   styleUrls: ['./usuarios.component.css']
 })
 export class UsuariosComponent implements OnInit {
-Usuarios=[
-{Nombre:"Salvador almaraz",Conversacion:"sdfsdfsdfdfsdfsd"},
-{Nombre:"Juan almaraz" ,Conversacion:"sdfsdfsdfdfsdfsd"},
-{Nombre:"Pedro almaraz" ,Conversacion:"sdfsdfsdfdfsdfsd"},
-{Nombre:"Luis almaraz" ,Conversacion:"sdfsdfsdfdfsdfsd"},
-{Nombre:"Laura almaraz" ,Conversacion:"sdfsdfsdfdfsdfsd"},
-{Nombre:"Paco almaraz" ,Conversacion:"sdfsdfsdfdfsdfsd"},
-{Nombre:"Sara almaraz" ,Conversacion:"sdfsdfsdfdfsdfsd"}
-];
-filtro:string;
-  constructor() {  
+  Usuarios = [
+  {Nombre: 'Salvador almaraz', Conversacion: 'sdfsdfsdfdfsdfsd'},
+  {Nombre: 'Ricardo Hernandez', Conversacion: 'conversacion'},
+  {Nombre: 'Sergio guzman', Conversacion: 'conversacion'},
+  {Nombre: 'Alex Reyna', Conversacion: 'conversacion'},
+  {Nombre: 'Laura almaraz', Conversacion: 'conversacion'},
+  {Nombre: 'Paco almaraz', Conversacion: 'conversacion'},
+  {Nombre: 'Sara almaraz', Conversacion: 'conversacion'}
+  ];
+  filtro: string;
+  constructor() {
   }
   ngOnInit() {
   }
