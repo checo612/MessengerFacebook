952681d13a557e235096c5be8ca90d4cf9d02c95
Ricardo Alfonso Hernandez Sanchez <hernandez.ricardo@neoris.cxnetworks.net>
2018-08-04 09:07:01 -0500

    Get last menssage in conversation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MensajesComponent } from './components/mensajes/mensajes.component';
 import { ChatMensajesComponent } from './components/chat-mensajes/chat-mensajes.component';
 
 const routes: Routes = [
-  { path: 'mensaje/:user', component: ChatMensajesComponent }
+  // { path: 'mensaje/:user/:id', component: MensajesComponent }
+  { path: 'mensaje', component: MensajesComponent},
+  { path: 'mensaje/:user/:id', component: MensajesComponent}
 ];
 
 @NgModule({
--- src/app/components/chat-mensajes/chat-mensajes.component.ts
+++ src/app/components/chat-mensajes/chat-mensajes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UsuariosService } from '../../services/usuarios.service';
 
 @Component({
   selector: 'app-chat-mensajes',
@@ -6,30 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat-mensajes.component.css']
 })
 export class ChatMensajesComponent implements OnInit {
-
-  constructor() { }
-  Mensajes = [
-    { Id: '1', Mensajes: 'Hola' },
-    { Id: '2', Mensajes: 'Hola' },
-    { Id: '2', Mensajes: 'Que dice?' },
-    { Id: '1', Mensajes: 'Nada' },
-    { Id: '2', Mensajes: 'Jajaja' },
-    { Id: '1', Mensajes: 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing' },
-    { Id: '1', Mensajes: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' },
-    { Id: '2', Mensajes: 'jsj' },
-    { Id: '1', Mensajes: 'jaja' },
-    { Id: '2', Mensajes: 'lkiwdiowe' },
-    { Id: '2', Mensajes: 'lkiwdiowe' },
-    { Id: '1', Mensajes: 'mensajea' },
-    { Id: '1', Mensajes: 'asadsadfsadf' },
-    { Id: '2', Mensajes: 'lkiwdiowe' },
-    { Id: '1', Mensajes: 'mensajeasdasadssadfsadf' },
-    { Id: '1', Mensajes: 'asadsadfsadf' },
-    { Id: '2', Mensajes: 'lkiwdiowe' },
-    { Id: '1', Mensajes: 'mensajeasdasadssadfsadf' },
-    { Id: '1', Mensajes: 'asadsadfsadf' }];
+  public idUser: string;
+  constructor(private route: ActivatedRoute, private mensajesService: UsuariosService) { }
 
   ngOnInit() {
+  }
+
+  getIdUser() {
+    this.idUser = this.route.snapshot.paramMap.get('id');
+    return this.idUser;
+  }
+
+  public getMensajes() {
+    return this.mensajesService.getMensajes();
   }
 
 }
